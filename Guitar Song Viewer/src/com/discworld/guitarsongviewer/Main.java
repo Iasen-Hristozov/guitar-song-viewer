d20a4c46aba29fed538ba6d88b5c01d22a55e78c
iasen.hristozov@gmail.com <iasen.hristozov@gmail.com@fb30f497-1158-6c23-777f-da489658f054>
2013-09-04 10:37:31 +0000

    Some changes in file managment.

--- Guitar Song Viewer/src/com/discworld/guitarsongviewer/Main.java
+++ Guitar Song Viewer/src/com/discworld/guitarsongviewer/Main.java
@@ -1,9 +1,7 @@
 package com.discworld.guitarsongviewer;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -81,183 +79,49 @@ public class Main extends FragmentActivity implements IDataExchange
    protected static final int   SHOW_PREFERENCES = 1,
                                 SHOW_ABOUT = 2,
                                 SHOW_OPEN = 3;
-   
-   private int iTextSize = 12,
+
+   private int iLinesNbr = 0,
+               iTextSize = 12,
                iEnuChordsShow = 0;
    
    private CSong oSong;
    
-   private TextView tvSong;
-   
    ArrayList<CTextVersesSet> alPages;
 
    LayoutInflater inflater;   //Used to create individual pages
+   
    ViewPager vp;   //Reference to class to swipe views
 
-   TextView tvText1;
-   
-   private int iLinesNbr;
-   
    private CPagerAdapter mPagerAdapter;
+   
+   TextView tvTitle,
+            tvChords;
+
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
       super.setContentView(R.layout.activity_main);
-      //      setContentView(R.layout.activity_main);
 
       updateFromPreferences();
       
-      File fSongsDir = new File(Environment.getExternalStorageDirectory().toString()+"/" + SONGS_FOLDER);
+      File fSongsDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/" + SONGS_FOLDER);
       fSongsDir.mkdirs();      
 
       oSong = getSongFromResources();
 //      getSongFromFile("");
       
-      
-      iLinesNbr = 0;
-      
-      TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
-      tvTitle.setText(oSong.sAuthor + " - " + oSong.sTitle);
-      TextView tvChords = (TextView) findViewById(R.id.tvChords);
-      tvChords.setText(null);
-      for(CChordsCouplet oChordsCouplet: oSong.alChords)
-      {
-         tvChords.append(oChordsCouplet.toString());
-         tvChords.append("\n\n");
-      }
+      tvTitle = (TextView) findViewById(R.id.tvTitle);
+      tvChords = (TextView) findViewById(R.id.tvChords);
+
       //initialsie the pager
       this.initialisePaging();      
-      
-//      this.iLinesNbr = 0;
-//      iTextSize = 24;
-//      initialisePaging();      
-
-      
    }
 
-   
-   private CSong getSong(XmlPullParser xmlSong)
-   {
-      CSong oSong = new CSong();
-      
-//      XmlResourceParser xmlSong = getResources().getXml(R.xml.antonina);
-
-      CChordsCouplet oChordsCouplet = new CChordsCouplet();
-      CChordsLine oChordsLine = new CChordsLine();
-      CChord oChord;
-    
-       CTextLine oTextLine = new CTextLine(); 
-       CTextVerse oTextVerse = new CTextVerse();
-     
-       int eventType = -1;
-       int iEnuTextType = 0;
-       while (eventType != XmlPullParser.END_DOCUMENT) 
-       {
-          if(eventType == XmlPullParser.START_TAG) 
-          { 
-             String strNode = xmlSong.getName();
-             if(strNode.equals(TAG_SONG))
-             {
-                oSong.sTitle = xmlSong.getAttributeValue(null, ATR_TITLE);
-                oSong.sAuthor = xmlSong.getAttributeValue(null, ATR_AUTHOR);
-              
-                if(xmlSong.getAttributeValue(null, ATR_LANGUAGE).equalsIgnoreCase(LNG_ENGLISH))
-                   oSong.iEnuLanguage = ENU_LNG_ENGLISH;
-                else if(xmlSong.getAttributeValue(null, ATR_LANGUAGE).equalsIgnoreCase(LNG_BULGARIAN))
-                   oSong.iEnuLanguage = ENU_LNG_BULGARIAN;
-                else if(xmlSong.getAttributeValue(null, ATR_LANGUAGE).equalsIgnoreCase(LNG_RUSSIAN))
-                   oSong.iEnuLanguage = ENU_LNG_RUSSIAN;
-             }
-             else if(strNode.equals(TAG_CHORDS_COUPLET))
-             {
-                oChordsCouplet = new CChordsCouplet();
-                oChordsCouplet.sID = xmlSong.getAttributeValue(null, ATR_ID);
-             }
-             else if(strNode.equals(TAG_CHORDS_LINE))
-             {
-                oChordsLine = new CChordsLine();
-                iEnuTextType = ENU_TEXT_TYPE_CHORDS;
-             }
-             else if(strNode.equals(TAG_TEXT_LINE))
-             {
-                oTextLine = new CTextLine();
-                iEnuTextType = ENU_TEXT_TYPE_TEXT;
-             }
-             else if(strNode.equals(TAG_TEXT_VERSE))
-             {
-                oTextVerse = new CTextVerse();
-                oTextVerse.sChordsCoupletID = xmlSong.getAttributeValue(null, ATR_ID_CHORD_COUPLET);
-                String sIsChorus = xmlSong.getAttributeValue(null, ATR_ID_IS_CHORUS);
-                if(sIsChorus == null)
-                   oTextVerse.bIsChorus = false;
-                else
-                   oTextVerse.bIsChorus = Integer.parseInt(sIsChorus) == 1;
-             }
-//             else
-//                continue;
-                
-          }
-          else if(eventType == XmlPullParser.END_TAG)
-          {
-             String strNode = xmlSong.getName();
-             if(strNode.equals(TAG_CHORDS_LINE))
-                oChordsCouplet.alChordsLines.add(oChordsLine);
-             else if(strNode.equals(TAG_CHORDS_COUPLET))
-                oSong.alChords.add(oChordsCouplet);
-             else if(strNode.equals(TAG_TEXT_LINE))
-                oTextVerse.addTextLine(oTextLine);
-             else if(strNode.equals(TAG_TEXT_VERSE))
-                oSong.oText.alTextVerses.add(oTextVerse);
-//                oSong.alText.add(oTextVerse);
-                
-          }
-        
-          else if(eventType == XmlPullParser.TEXT)
-          {
-             if(iEnuTextType == ENU_TEXT_TYPE_CHORDS)
-             {
-                String ss = xmlSong.getText();
-                String[] sChords = ss.split(" ");
-                for(int i = 0; i < sChords.length; i++)
-                {
-                   oChord = new CChord(sChords[i]);
-                   oChordsLine.addChord(oChord);
-                }
-             }
-             else if(iEnuTextType == ENU_TEXT_TYPE_TEXT)
-             {
-                oTextLine.sTextLine = xmlSong.getText();
-             }
-          }
-   
-          try 
-          {
-//             if(eventType == XmlPullParser.END_TAG)
-//                eventType = xmlSong.nextTag();
-//             else
-                eventType = xmlSong.next();
-//                eventType = xmlSong.nextToken();
-          } 
-          catch (XmlPullParserException e) 
-          {
-             e.printStackTrace();
-          } 
-          catch (IOException e) 
-          {
-             e.printStackTrace();
-          }
-       }
-       
-       return oSong;
-   }
-   
    private CSong getSongNew(XmlPullParser xmlSong)
    {
       CSong oSong = new CSong();
       
-//      XmlResourceParser xmlSong = getResources().getXml(R.xml.antonina);
-
       CChordsCouplet oChordsCouplet = new CChordsCouplet();
       CChordsLine oChordsLine = new CChordsLine();
       CChord oChord;
@@ -270,7 +134,6 @@ public class Main extends FragmentActivity implements IDataExchange
 //         xmlSong.nextTag();
 //         xmlSong.next();
          xmlSong.next();
-         String sNode = xmlSong.getName();
          xmlSong.require(XmlPullParser.START_TAG, null, TAG_SONG);
          oSong.sTitle = xmlSong.getAttributeValue(null, ATR_TITLE);
          oSong.sAuthor = xmlSong.getAttributeValue(null, ATR_AUTHOR);
@@ -354,7 +217,6 @@ public class Main extends FragmentActivity implements IDataExchange
       return oSong;
    }   
    
-   
    private CSong getSongFromResources()
    {
       XmlResourceParser xmlSong = getResources().getXml(R.xml.antonina);
@@ -380,43 +242,24 @@ public class Main extends FragmentActivity implements IDataExchange
 //   private CSong getSongFromFile(String sFile)
    private void getSongFromFile(String sFile) 
    {
-//      CSong oSong = null;
-        
       try
       {
-//         FileInputStream oFileInputStream;
-//         oFileInputStream = new FileInputStream(sFile);
-//         
-//         XmlPullParserFactory oXmlPullParserFactory = XmlPullParserFactory.newInstance();
-//         oXmlPullParserFactory.setNamespaceAware(true);
-//         
-//         XmlPullParser xmlSong = oXmlPullParserFactory.newPullParser();
-//
-//         xmlSong.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-////         xmlSong.setInput(oFileInputStream, null);
-//         xmlSong.setInput(oFileInputStream, "UTF_8");
-
-         
-//         FileReader in = new FileReader(sFile);
-//         
-//         XmlPullParserFactory oXmlPullParserFactory = XmlPullParserFactory.newInstance();         
-//         XmlPullParser xmlSong = oXmlPullParserFactory.newPullParser();
-//
-//         xmlSong.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-//         xmlSong.setInput(in);
-         
          XmlPullParserFactory oXmlPullParserFactory = XmlPullParserFactory.newInstance();
          
          oXmlPullParserFactory.setValidating(false);
          XmlPullParser xmlSong = oXmlPullParserFactory.newPullParser();
 
          xmlSong.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-//         xmlSong.setFeature(XmlPullParser.FEATURE_VALIDATION, true);
 
          InputStream in = getApplicationContext().getAssets().open("antonina.xml");
          xmlSong.setInput(in, null);
          
-         oSong = getSongNew(xmlSong);  
+         oSong = getSongNew(xmlSong);
+         
+         setTitle();
+         
+         if(iEnuChordsShow == ENU_SHOW_CHORDS_ALL)
+            setChords();
          
       } catch(FileNotFoundException e)
       {
@@ -510,18 +353,16 @@ public class Main extends FragmentActivity implements IDataExchange
       }
    }
    
-   Bundle bundle;
-   Fragment fPage;
-   
    private void initialisePaging() 
    {
       Fragment fPage;
+      Bundle bundle;
+      
       List<Fragment> fragments = new Vector<Fragment>();
       fPage = Fragment.instantiate(this, CTextFragment.class.getName());
       bundle = new Bundle();
       bundle.putInt("index", 0);
       bundle.putInt("text_size", iTextSize);
-//      bundle.putParcelableArrayList("verse", oSong.oText.alTextVerses);
       fPage.setArguments(bundle);
       fragments.add(fPage);
       this.mPagerAdapter  = new CPagerAdapter(super.getSupportFragmentManager(), fragments);
@@ -530,20 +371,6 @@ public class Main extends FragmentActivity implements IDataExchange
       pager.setAdapter(this.mPagerAdapter);
    }
 
-   private void initialisePaging1() 
-   {
-      Fragment fPage;
-      fPage = Fragment.instantiate(this, CTextFragment.class.getName());
-      bundle = new Bundle();
-      bundle.putInt("index", 0);
-      bundle.putInt("text_size", iTextSize);
-//      bundle.putParcelableArrayList("verse", oSong.oText.alTextVerses);
-      fPage.setArguments(bundle);
-      this.mPagerAdapter.addItem(fPage);
-      this.mPagerAdapter.notifyDataSetChanged();
-   }
-   
-   
    @Override
    public void setLinesNbr(int iLinesNbr)
    {
@@ -556,66 +383,18 @@ public class Main extends FragmentActivity implements IDataExchange
          Fragment fPage;
          Bundle bundle;
          
-//         this.mPagerAdapter.deleteItem(0);
-//         this.mPagerAdapter.notifyDataSetChanged();
-
-//         ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPager);
-//         fPage = Fragment.instantiate(this, CTextFragment.class.getName());
-//         Object objectobject = mPagerAdapter.instantiateItem(mViewPager, 0);
-//         this.mPagerAdapter.destroyItem(mViewPager, 0, objectobject);
          for(int iPageNdx = 1; iPageNdx < alPages.size(); iPageNdx++)
          {
-             
-            
-            
-//            if(iPageNdx == 0)
-//            {
-//               bundle1.putInt("LinesNbr", iLinesNbr);
-//               bundle1.putParcelableArrayList("verse", alPages.get(iPageNdx).alTextVerses);
-//               fPage1.setArguments(bundle1);
-//               
-//               this.mPagerAdapter.updateItem(fPage1, iPageNdx);
-//            }
-//            else
-//            {
-               fPage = Fragment.instantiate(this, CTextFragment.class.getName());
-//               TextView tv = (TextView)fPage.getView().findViewById(R.id.tvText);
-               bundle = new Bundle();
-//                bundle.putString("edttext", "From Activity1111");
-//               bundle.putInt("LinesNbr", iLinesNbr);
-               bundle.putInt("index", iPageNdx);
-               bundle.putInt("text_size", iTextSize);
-//               bundle.putParcelableArrayList("verse", alPages.get(iPageNdx).alTextVerses);
-               fPage.setArguments(bundle);
-               this.mPagerAdapter.addItem(fPage);
-//               i = mPagerAdapter.getItemId(iPageNdx);
-//            }
+            fPage = Fragment.instantiate(this, CTextFragment.class.getName());
+            bundle = new Bundle();
+            bundle.putInt("index", iPageNdx);
+            bundle.putInt("text_size", iTextSize);
+            fPage.setArguments(bundle);
+            this.mPagerAdapter.addItem(fPage);
          }
          
          this.mPagerAdapter.notifyDataSetChanged();
       }
-      
-//      fPage = Fragment.instantiate(this, CTextFragment.class.getName());
-////      TextView tv = (TextView)fPage.getView().findViewById(R.id.tvText);
-//      bundle = new Bundle();
-//      bundle.putInt("LinesNbr", iLinesNbr);
-//      bundle.putInt("index", 0);
-//      fPage.setArguments(bundle);
-////      ((CTextFragment) fPage).setTextSize(12);
-//      mPagerAdapter.addItem(fPage);
-//      mPagerAdapter.notifyDataSetChanged();
-//      CTextFragment fPage1 = (CTextFragment) mPagerAdapter.getItem(0);
-//      fPage1.setTextSize(12);      
-      
-      
-//      CTextFragment fPage1 = (CTextFragment) mPagerAdapter.getItem(1);
-//      fPage1.setTextSize(1);
-      
-//      CTextFragment fPage2 = (CTextFragment) mPagerAdapter.getItem(2);
-//      mPagerAdapter.destroyItem(vp, 2, fPage2);
-//      mPagerAdapter.deleteItem(2);
-//      mPagerAdapter.notifyDataSetChanged();
-      
    }
 
    private void preparePages()
@@ -627,7 +406,6 @@ public class Main extends FragmentActivity implements IDataExchange
       
       CTextVersesSet oPage = new CTextVersesSet();
    
-//           ArrayList<CTextVersesSet> alPages = new ArrayList<CTextVersesSet>();
       alPages = new ArrayList<CTextVersesSet>();
       for(iTextVerseNdx = 0; iTextVerseNdx < oSong.oText.alTextVerses.size(); iTextVerseNdx++)
       {
@@ -688,6 +466,19 @@ public class Main extends FragmentActivity implements IDataExchange
    {
       return iTextSize;
    }
-
-
+   
+   private void setTitle()
+   {
+      tvTitle.setText(oSong.sAuthor + " - " + oSong.sTitle);
+   }
+   
+   private void setChords()
+   {
+      tvChords.setText(null);
+      for(CChordsCouplet oChordsCouplet: oSong.alChords)
+      {
+         tvChords.append(oChordsCouplet.toString());
+         tvChords.append("\n\n");
+      }
+   }
 }
